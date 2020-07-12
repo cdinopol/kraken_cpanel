af874aa57c58514a0f049e51c076d29170b42ed1
Carlo Activ <carlo@activ.com.sg>
2020-07-12 09:57:03 +0800

    access token expire checker, remove weird donation error message

--- app/js/y.js
+++ app/js/y.js
@@ -5,16 +5,20 @@ if (!session) {
 	localStorage.setItem("me_url", window.location.href);
     window.location.href = "/";
 } else {
+	session = JSON.parse(session);
+	
+	// check for session validity
+	check_session_validity();
 
-	// if theres me url
+	// if theres me url (this is from remembering url inside /me when directly accessed without current session)
 	let me_url = localStorage.getItem("me_url");
 	if (me_url) {
 		localStorage.removeItem("me_url");
 		window.location.replace(me_url);
 	}
 
-	document.addEventListener("DOMContentLoaded", function(event) { 
-	  	session = JSON.parse(session);
+	// auto load values
+	document.addEventListener("DOMContentLoaded", function(event) {
 		document.querySelector('#user_username').innerHTML = session.user.username;
 		document.querySelector('#user_email').innerHTML = session.user.email;
 		
@@ -26,4 +30,13 @@ if (!session) {
 			document.querySelector('#email').value = session.user.email;
 		}
 	});
+}
+
+function check_session_validity() {
+	var now = Math.round(new Date().getTime() / 1000.0);
+	var expiry = session.expires_in_epoch;
+	if (now >= expiry) {
+		localStorage.removeItem(SESSION);
+    	window.location.href = "/";
+	}
 }
